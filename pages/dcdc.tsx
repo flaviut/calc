1cf60e15c11d1a0076a642723795be56649ef236
Flaviu Tamas <tamasflaviu@gmail.com>
2019-11-07 22:24:55 -0500

    Add actual calculations

--- pages/dcdc.tsx
+++ pages/dcdc.tsx
@@ -4,13 +4,13 @@ import { useState, useMemo, ReactElement } from 'react'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 
-import { unit } from 'mathjs'
+import * as mathjs from 'mathjs'
 
 type UnitFieldValue = InvalidUnitFieldValue | ValidUnitFieldValue
 
 function parseInput(input: string, acceptedUnit: string): UnitFieldValue {
   try {
-    const parsedUnit = unit(input).toNumber(acceptedUnit)
+    const parsedUnit = mathjs.evaluate(input).to(acceptedUnit)
     return new ValidUnitFieldValue(input, parsedUnit)
   } catch (error) {
     return new InvalidUnitFieldValue(input, error.message)
@@ -19,29 +19,35 @@ function parseInput(input: string, acceptedUnit: string): UnitFieldValue {
 
 class InvalidUnitFieldValue {
   constructor(public readonly textValue: string, public readonly error: string) { }
+  isError(): boolean { return true }
 }
 
 class ValidUnitFieldValue {
-  constructor(public readonly textValue: string, public readonly value: number) { }
+  constructor(public readonly textValue: string, public readonly value: mathjs.Unit) { }
+  isError(): boolean { return false }
 }
 
 const CalculatorField: React.FunctionComponent<{
-  math: ReactElement,
-  defaultValue: string,
+  label: ReactElement,
+  name: string,
   acceptedUnit: string,
-}> = ({ math, defaultValue, acceptedUnit }) => {
-  const [fieldValue, setFieldValue] = useState<UnitFieldValue>(
-    parseInput(defaultValue, acceptedUnit))
 
-  const isError = fieldValue instanceof InvalidUnitFieldValue
+  scope: { [name: string]: UnitFieldValue },
+  setScope: React.Dispatch<React.SetStateAction<any>>
+}> = ({ label, name, acceptedUnit, scope, setScope }) => {
+  const fieldValue = scope[name]
+  const isError = fieldValue.isError()
 
   const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = useMemo(() => (event) => {
-    setFieldValue(parseInput(event.target.value, acceptedUnit))
-  }, [setFieldValue, acceptedUnit])
+    setScope({
+      ...scope,
+      [name]: parseInput(event.target.value, acceptedUnit)
+    })
+  }, [setScope, scope, acceptedUnit])
 
   return (
     <React.Fragment>
-      <Grid item xs={12} sm={4}>{math}</Grid>
+      <Grid item xs={12} sm={4}>{label}</Grid>
       <Grid item xs={12} sm={8}>
         <TextField
           error={isError}
@@ -52,7 +58,55 @@ const CalculatorField: React.FunctionComponent<{
     </React.Fragment>)
 }
 
+const ResultField: React.FunctionComponent<{
+  label: string,
+  equation: string,
+  scope: { [name: string]: UnitFieldValue },
+}> = ({ label, equation, scope }) => {
+
+  // map each field in the state to a simple number (or undefined)
+  // this is so that we can use it to evaluate the field value
+  const valueScope: { [name: string]: mathjs.Unit } =
+    Object.keys(scope).reduce((p, c) => {
+      const fieldValue = scope[c]
+      return ({
+        ...p,
+        [c]: fieldValue.isError() ?
+          undefined :
+          (fieldValue as ValidUnitFieldValue).value
+      })
+    }, {})
+
+  let fieldValue = "undef"
+  try {
+    fieldValue = mathjs.evaluate(equation, valueScope).toString()
+    // parse and format again. There's some kind of flag in the unit object
+    // that prevents the prefix from being re-interpreted otherwise
+    fieldValue = mathjs.unit(fieldValue).format({ precision: 3 })
+  } catch (err) {
+    fieldValue = `Undefined: ${err.message}`
+  }
+
+  return (<React.Fragment>
+    <Grid item xs={12} sm={4}>{label}</Grid>
+    <Grid item xs={12} sm={8}>{fieldValue}</Grid>
+  </React.Fragment>)
+}
+
 const DcDcCalculatorPage: React.FunctionComponent = () => {
+  const [scope, setScope] = useState({
+    freq: parseInput("100 kHz", "Hz"),
+    max_v_in: parseInput("12 V", "V"),
+    min_v_in: parseInput("5 V", "V"),
+    max_v_out: parseInput("24 V", "V"),
+    min_v_out: parseInput("20 V", "V"),
+    min_i_out: parseInput("1 A", "A"),
+    max_v_pkpk: parseInput("0.05 V", "V"),
+  })
+
+  const min_duty_eq = "1 - (min_v_in / max_v_out)"
+  const max_duty_eq = "1 - (max_v_in / min_v_out)"
+  const min_inductor_size = `(${max_duty_eq}) * min_v_in * (1 - (${max_duty_eq})) / (freq * 2 * min_i_out)`
 
   return <Layout title="DC-DC Converter Calculator">
     <h1>DC-DC Converter Calculator</h1>
@@ -61,25 +115,53 @@ const DcDcCalculatorPage: React.FunctionComponent = () => {
     <h2>Inputs</h2>
     <Grid container>
       <CalculatorField
-        math={<span>Max V<sub>in</sub></span>}
-        defaultValue="5 V" acceptedUnit='V' />
+        label={<span>Frequency</span>}
+        name="freq"
+        acceptedUnit='Hz' scope={scope} setScope={setScope} />
       <CalculatorField
-        math={<span>Min V<sub>in</sub></span>}
-        defaultValue="12 V" acceptedUnit='V' />
+        label={<span>Max V<sub>in</sub></span>}
+        name="max_v_in"
+        acceptedUnit='V' scope={scope} setScope={setScope} />
       <CalculatorField
-        math={<span>Max V<sub>out</sub></span>}
-        defaultValue="12 V" acceptedUnit='V' />
+        label={<span>Min V<sub>in</sub></span>}
+        name="min_v_in"
+        acceptedUnit='V' scope={scope} setScope={setScope} />
       <CalculatorField
-        math={<span>Min V<sub>out</sub></span>}
-        defaultValue="12 V" acceptedUnit='V' />
+        label={<span>Max V<sub>out</sub></span>}
+        name="max_v_out"
+        acceptedUnit='V' scope={scope} setScope={setScope} />
       <CalculatorField
-        math={<span>Min I<sub>out</sub></span>}
-        defaultValue="12 A" acceptedUnit='A' />
+        label={<span>Min V<sub>out</sub></span>}
+        name="min_v_out"
+        acceptedUnit='V' scope={scope} setScope={setScope} />
       <CalculatorField
-        math={<span>Max V<sub>pk-pk</sub></span>}
-        defaultValue="12 V" acceptedUnit='V' />
+        label={<span>Min I<sub>out</sub></span>}
+        name="min_i_out"
+        acceptedUnit='A' scope={scope} setScope={setScope} />
+      <CalculatorField
+        label={<span>Max V<sub>pk-pk</sub></span>}
+        name="max_v_pkpk"
+        acceptedUnit='V' scope={scope} setScope={setScope} />
     </Grid>
     <h2>Results</h2>
+    <Grid container>
+      <ResultField
+        label="Max Duty Cycle"
+        equation={max_duty_eq}
+        scope={scope} />
+      <ResultField
+        label="Min Duty Cycle"
+        equation={min_duty_eq}
+        scope={scope} />
+      <ResultField
+        label="Min Inductor Size"
+        equation={min_inductor_size}
+        scope={scope} />
+      <ResultField
+        label="Peak Inductor Current"
+        equation={`(max_v_in * (${max_duty_eq})) / (freq * (${min_inductor_size}))`}
+        scope={scope} />
+    </Grid>
   </Layout>
 }
 
