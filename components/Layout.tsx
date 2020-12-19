325090a0e78dcb52fdb4d7a9d7da4e317109ae2e
Flaviu Tamas <tamasflaviu@gmail.com>
2020-12-19 16:19:49 -0500

    Fix build falure

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,23 +2,19 @@ import * as React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 
-import '../public/static/all.scss'
 import { Grid } from './Grid'
 
 const NavBar: React.FunctionComponent = () => (
-  <header className="navbar" style={{
-  }}>
+  <header className="navbar">
     <section className="navbar-section">
-      <Link href="/"><a className="btn btn-link">flaviutamas.com</a></Link>{" "}
-    </section>
-    <section className="navbar-section">
-      <Link href="/calc"><a className="btn btn-link">calc</a></Link>{" "}
+      <Link href="/"><a className="navbar-brand mr-2">flaviutamas.com</a></Link>
+      <Link href="/calc"><a className="btn btn-link">calc</a></Link>
     </section>
   </header>)
 
 const Footer: React.FunctionComponent = () => (
   <footer>
-    © 2019 Flaviu Tamas,{" "}
+    © 2020 Flaviu Tamas,{" "}
     <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">CC-BY-4.0</a> or{" "}
     <a href="http://www.apache.org/licenses/LICENSE-2.0" rel="license">Apache 2.0</a>
   </footer>)
