4195d4a0d2d5777804e723760e94d9f35b7db2bd
Flaviu Tamas <tamasflaviu@gmail.com>
2020-12-19 16:06:14 -0500

    Bump versions

--- next.config.js
+++ next.config.js
@@ -2,5 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 const withImages = require('next-images')
-const withSass = require('@zeit/next-sass')
-module.exports = withSass(withImages(withBundleAnalyzer({})))
+module.exports = withImages(withBundleAnalyzer({}))
